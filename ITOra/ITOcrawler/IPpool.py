12f2b5308ddc3a630cf046fb1787a556277432bb
Chenghao Qian <Jkob@localhost.localdomain>
2016-09-25 15:41:04 -0500

    let's see

--- ITOra/ITOcrawler/IPpool.py
+++ ITOra/ITOcrawler/IPpool.py
@@ -6,16 +6,21 @@ def ipCrawl():
         print("哎呦，IP代理池空了，准备一些吧")
         proxylist = []
         page = 1
-        while page <= 15:
-                urls = "http://www.kuaidaili.com/proxylist/%d/"%page
+        while page <= 2:
+                urls = "http://www.xicidaili.com/nn/%d"%page
+        #        urls = "http://www.kuaidaili.com/proxylist/%d/"%page
                 request = urllib.request.Request(urls,headers =header)
                 content = urllib.request.urlopen(request)
-
+                
                 buff = content.read().decode('utf-8')
-                reprox = re.compile(r'<td data-title="IP">(.+?)</td>.*?<td data-title="PORT">(.+?)</td>.*?<td data-title="匿名度">(.+?)</td>.*?<td data-title="类型">(.+?)</td>.*?<td data-title="get/post支持">(.+?)</td>.*?<td data-title="位置">(.+?)</td>.*?<td data-title="响应速度">(.+?)</td>.*?<td data-title="最后验证时间">(.+?)</td>',re.S)
+                print(buff)
+                reprox = re.compile(r'<td class="country"><img.*?<td>(.+?)</td>.*?<td>(.+?)</td><td class.*?<td>(.+?)</td>.*?</tr>',re.S)
+                #reprox = re.compile(r'<td data-title="IP">(.+?)</td>.*?<td data-title="PORT">(.+?)</td>.*?<td data-title="匿名度">(.+?)</td>.*?<td data-title="类型">(.+?)</td>.*?<td data-title="get/post支持">(.+?)</td>.*?<td data-title="位置">(.+?)</td>.*?<td data-title="响应速度">(.+?)</td>.*?<td data-title="最后验证时间">(.+?)</td>',re.S)
                 proxlist = re.findall(reprox,buff)
                 for prox in proxlist:
                         proxylist.append(prox)
+                        for each in prox:
+                                print(each)
                 page+=1
         print("大量IP准备完毕")
         httplist = [httpprox for httpprox in proxylist if 'HTTPS' not in httpprox[3]]
--- ITOra/ITOcrawler/ITOcraw.py
+++ ITOra/ITOcrawler/ITOcraw.py
@@ -1,66 +1,116 @@
-import requests,pickle,random,re,time
-import IPpool
+import requests,pickle,random,re,time,threading
+import IProxypool
+proxypool = IProxypool.Proxypool()
+companylist = []
+lock = threading.Lock()
+page = 0
+class ITOrange(threading.Thread):
+
+        def __init__(self,thread_id,pages,func):
+                super(ITOrange,self).__init__()
+                self.thread_id = thread_id
+                self.func = func
+                self.pages = pages
+        def run(self):
+                print("线程 %d 开始"%self.thread_id)
+                self.func(self.thread_id,self.pages)
+                print("线程 %d 完成任务"%self.thread_id)
+
+def worker(parafunc):
+        def in_worker(thread_id,pages):
+                header = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+                proxy = None 
+                global proxypool
+                global companylist
+                global page
+                page_number = page
+                pages,urls,repattern,thread_id = parafunc(thread_id,pages)
+                while page_number < pages:
+                        
+                        url = eval(urls)
+                        if page == 125:
+                                continue
+                        try:
+                                request = requests.get(url,headers = header,proxies= proxy,timeout=10)
+                                print(request.status_code) 
+                                print("Thread %d get the page No.%d"%(thread_id,page_number))
+                                if request.status_code != 200:
+                                        raise ConnectError("Cannot connect to server")
+                                request.encoding = 'utf8'
+                                buff = request.text
+                                recomp = re.compile(repattern,re.S)
+                        except:
+                                lock.acquire()
+                                if proxypool:
+                                        proxy = get_a_proxy(proxypool,thread_id)
+                                else:
+                                        proxypool = IProxypool.Proxypool()               
+                                        proxy = get_a_proxy(proxypool,thread_id)
+                                        time.sleep(60)
+                                lock.release()
+                                continue 
+                        complist = re.findall(recomp,buff)
+                        for company in complist:
+                                compform = [company[0]+'\n',company[1]+'\n',company[2]+'\t',company[3]+'\t',company[4].strip()+'\t'+company[5]+'\n\n\n'] 
+                                if compform not in companylist:
+                                        companylist.append(compform)
+                        
+                        print("Thread %d get page %d Done!"%(thread_id,page_number))
+                        lock.acquire()
+                        page+=1
+                        page_number=page
+                        lock.release()
+                        #sand = random.randint(1,3)
+                        #time.sleep(3)
+        return in_worker
+
+@worker
+def ITOcomp(thread_id,pages):
+        urls = '"http://www.itjuzi.com/company?page=%d"%page_number'
+        #repattern = r'<p class="title">.*?<span>(.+?)</span>.*?<p class="des"><span>(.+?)</span>.*?<a href=.*?>(.+?)</a>.*?<a href=.*?>(.+?)</a>.*?<i class="cell date">(.+?)</i>.*?<span class=.*?>(.+?)</span>'
+        #repattern=r'<p class="title">.*?<span>(.+?)</span>.*?<a href.*?>(.+?)</a>.*?<a href.*?>(.+?)</a>.*?date">(.+?)</i>.*?<span.*?>(.+?)</span>'
+        
+        repattern = r'<p class="title">.*?<span>(.+?)</span>.*?<p class="des">.*?<span>(.+?)</span>.*?</p>.*?<span.*?<a href.*?>(.+?)</a>.*?</span>.*?<span.*?<a href.*?>(.+?)</a>.*?</span>.*?<i.*?>(.*?)</i>.*?<a.*?>(.*?)</a>'
+        return pages,urls,repattern,thread_id
 
 
-def get_a_proxy(pool):
-        if pool[0]:
+
+
+
+
+
+def compCraw():
+        global page
+        print("* * * * * Welcome to IT Citus Crawler * * * * *")
+        pages = int(input("please enter the pages you want:"))
+        print("主线程开始...")
+        threads = [ITOrange(i,pages,ITOcomp) for i in range(1,11)]
+        for thread in threads:
+                time.sleep(1)
+                page+=1
+                thread.start()
+        for t in threads:
+                t.join()
+
+        print("主线程结束,正在保存...")
+        compdata = open("ITOcompany.pkl","wb")
+        pickle.dump(companylist,compdata)
+        print("保存完毕！")
+        
+        
+
+
+
+
+
+
+def get_a_proxy(pool,thread_id):
+        if pool:
                 proxy = {"http":"http://%s:%s"%(pool[0][0],pool[0][1])}
-                print("切换到ip地址:%s"%pool[0][0])
+                print("线程 %d 切换到ip地址:%s"%(thread_id,pool[0][0]))
         else:
                 proxy = None
         del pool[0]        
         return proxy 
 
-
-def companyCrawl():
-        print("* * * * * Welcome to IT Citus Crawler * * * * *")
-        pages = int(input("please enter the pages you want:"))
-        proxypool = IPpool.ipCrawl()
-        proxy = None
-        #proxy = get_a_proxy(proxypool)
-        headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}        
-        compnum = 0
-        #Please change the base url here
-        companylist = []
-        page = 1
-        while page <= pages:
-                urls = "http://www.itjuzi.com/company?page=%d"%page
-                try:
-                        request = requests.get(urls,headers = headers,proxies = proxy,timeout=10)
-                        if request.status_code != 200:
-                                raise ConnectError("Cannot connect to server") 
-                        request.encoding = 'utf8'
-                        buff = request.text
-                        recomp = re.compile(r'<p class="title">.*?<span>(.+?)</span>.*?<p class="des"><span>(.+?)</span>.*?<a href=.*?>(.+?)</a>.*?<a href=.*?>(.+?)</a>.*?<i class="cell date">(.+?)</i>.*?<span class=.*?>(.+?)</span>',re.S)
-                        complist = re.findall(recomp,buff) 
-
-                except:
-                        if proxypool:
-                                proxy = get_a_proxy(proxypool)
-                                
-                        else:
-                                proxypool = IPpool.ipCrawl()
-                                proxypool.insert(0,None)
-                                print(proxypool) 
-                                proxy = get_a_proxy(proxypool)
-                                time.sleep(60)
-                        time.sleep(5)
-                        continue
-                                                
-                for company in complist:
-                        compform = [company[0]+'\n',company[1]+'\n',company[2]+'\t',company[3]+'\t',company[4].strip()+'\t','\t'+company[5]+'\n\n\n']
-                        if compform in companylist:
-                                continue
-                        print("No.%d company info crawled"%compnum)
-                        companylist.append(compform)
-                        compnum += 1
-                        for each in compform:
-                                print(each)                        
-                print('---------------Page %d finished ------------------'%page)
-                page += 1
-                sand = random.randint(1,3)
-                time.sleep(sand)
-        compdata = open("ITOcompany.pkl","wb")
-        pickle.dump(companylist,compdata)
-
-companyCrawl()
+compCraw()
